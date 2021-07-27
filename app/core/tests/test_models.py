6cd1f4790dd5584a8c704405e07ab0367df87973
Andrew Tsai <andrew.tsai@viasat.com>
2021-07-27 07:23:14 -0700

    Add list tag feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,12 +1,18 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from core import models
+
+
+def sample_user(email='test@example.com', password='testpass'):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email, password)
 
 
 class ModelTests(TestCase):
 
     def test_create_user_with_email_succesful(self):
         """Test creating a new user with an email is successful"""
-        email = 'test@viasat.com'
+        email = 'test@example.com'
         password = 'test123'
         user = get_user_model().objects.create_user(
             email=email,
@@ -18,7 +24,7 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
-        email = 'test@ViaSat.com'
+        email = 'test@eXaMplE.com'
         user = get_user_model().objects.create_user(
             email, 'test123'
         )
@@ -32,9 +38,17 @@ class ModelTests(TestCase):
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
         user = get_user_model().objects.create_superuser(
-            'test@viasat.com',
+            'test@example.com',
             'test123'
         )
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test the tag string representation"""
+        tag = models.Tag.objects.create(
+            user=sample_user(),
+            name='Vegan'
+        )
+        self.assertEqual(str(tag), tag.name)
