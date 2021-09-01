9c6cfdbcf155017d5d93704a291968a5589e3c40
Andrew Tsai <andrew.tsai@viasat.com>
2021-08-31 21:30:20 -0700

    Add feature to filter recipes

--- app/core/models.py
+++ app/core/models.py
@@ -61,7 +61,7 @@ class Ingredient(models.Model):
     """Ingredient to be used in a recipe"""
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
+        settings.AUTH_USER_MODEL,   
         on_delete=models.CASCADE
     )
 
